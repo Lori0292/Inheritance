a06a2bb317ac3c47930998d2cc3a352a632cc1ad
Lori0292 <562332@my.cuhsd.org>
2025-03-25 07:33:14 -0700

    Create ShapesTester.java

---
+++ ShapesTester.java
@@ -0,0 +1,14 @@
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        Shapes shape1 = new Shapes("Block", "Pink");
+        System.out.println(shape1.toString());
+        
+        Shapes shape2 = new Triangle("Blue", 5, 7, 3);
+        System.out.println(shape2.toString());
+        
+        Shapes shape3 = new Star("Yellow", 5, 10, 5);
+        System.out.println(shape3.toString());
+    }
+}
